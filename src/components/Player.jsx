44d8c0ef73191d22fc454ba5857136f630531cd9
Aguss <agustinampo80@gmail.com>
2025-04-27 20:20:49 -0300

    React audio commit

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,18 @@
-import { useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 const Pause = () => {
-    <svg role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16"><path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path></svg>
+    return (
+        <svg role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16"
+        ><path
+            d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"
+        ></path></svg>
+    )
 }
 
 const Play = () => {
-    <svg viewBox="0 0 24 24" class="h-8 w-8" fill="currentColor"><path fill="currentColor" d="M8 5.14v14l11-7-11-7z"></path></svg>
+    return(
+        <svg role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 24 24"><path d="M8 5.14v14l11-7-11-7z"></path></svg>
+    )
 }
 
 export function Player () {
@@ -13,11 +20,14 @@ export function Player () {
     const [currentSong, setCurrentSong] = useState(null)
     const audioRef = useRef()
 
+    useEffect(() => {
+        audioRef.current.src = `/music/1/01.mp3`
+    }, [])
+
     const handleClick = () => {
         if(isPlaying){
             audioRef.current.pause()
         }else{
-            audioRef.current.src = `/music/1/01.mp3`
             audioRef.current.play()
             audioRef.current.volume = 0.1
         }
