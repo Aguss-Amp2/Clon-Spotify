4e40796d10471ca871f1dfebc7f32330560f952c
Aguss <agustinampo80@gmail.com>
2025-04-27 19:09:53 -0300

    Svelte commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import svelte from '@astrojs/svelte';
+
 export default defineConfig({
   output: 'server',
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [svelte()]
 });
---
+++ src/components/Greeting.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    const currentTime = new Date()
+    const currentHour = currentTime.getHours()
+
+    let greeting = ""
+
+    if (currentHour < 12){
+        greeting = "Buenos Dias"
+    }else if (currentHour > 18){
+        greeting = "Buenos Tardes"
+    }else {
+        greeting = "Buenos Noches"
+    }
+</script>
+
+<h1 class="text-3xl font-bold">{greeting}</h1>
