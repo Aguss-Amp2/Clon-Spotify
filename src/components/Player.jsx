30a5af10fee9d92456d4d5eb0b6365fd3dd97ed9
Aguss <agustinampo80@gmail.com>
2025-04-28 12:49:25 -0300

    React hover commit

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,22 +1,20 @@
 import { useEffect, useRef, useState } from "react"
+import { usePlayerStore } from "../store/playerStore"
 
-const Pause = () => {
+export const Pause = () => {
     return (
-        <svg role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16"
-        ><path
-            d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"
-        ></path></svg>
+        <svg role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16"><path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path></svg>
     )
 }
 
-const Play = () => {
+export const Play = () => {
     return(
-        <svg role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 24 24"><path d="M8 5.14v14l11-7-11-7z"></path></svg>
+        <svg role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 24 24"><path d="m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606z"></path></svg>
     )
 }
 
 export function Player () {
-    const [isPlaying, setIsPlaying] = useState(false)
+    const { isPlaying, setIsPlaying } = usePlayerStore(state => state)
     const [currentSong, setCurrentSong] = useState(null)
     const audioRef = useRef()
 
