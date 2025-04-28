640c89a1a813f92e4ace57d9b9424b78a26aa17a
Aguss <agustinampo80@gmail.com>
2025-04-28 18:15:18 -0300

    React Table commit

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -137,7 +137,7 @@ export function Player () {
     }
 
     return(
-        <div className="flex flex-row justify-between w-full px-4 z-50">
+        <div className="flex flex-row justify-between w-full px-1 z-50">
             <div className="w-[180px]">
                 <CurrentSong {...currentMusic.song}/>
             </div>
